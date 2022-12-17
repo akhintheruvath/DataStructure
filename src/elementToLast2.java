5b2e60591963100cc1c89f41ea758be162943739
Akhin T <112750354+akhintheruvath@users.noreply.github.com>
2022-12-17 18:37:29 +0530

    no changes

--- src/elementToLast2.java
+++ src/elementToLast2.java
@@ -2,17 +2,15 @@ public class elementToLast2 {
     public static void main(String[] args) {
         int[] arr = { 6, 1, 10, 8, 6, 10, 15, 10, 6, 10, 12, 6, 10, 30, 10 };
         int len = arr.length;
-        int n = len - 1;
         int j = len - 1;
         int i = 0;
-        while (i < n - 1) {
+        while (i < j) {
             if (arr[i] == 10) {
-                while (arr[j] == 10) {
+                while (arr[j] == 10 && i<j) {
                     --j;
                 }
                 arr[i] = arr[j];
                 arr[j] = 10;
-                n = j;
             }
             ++i;
         }
